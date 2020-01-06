1f152477a1a4c78ac258d01dcbfb2af1b17e09ea
Wojciech Default <wojciechklanowski@gmail.com>
2020-01-06 22:52:01 +0100

    logging and signing in start

--- AccountModule/Program.cs
+++ AccountModule/Program.cs
@@ -10,6 +10,42 @@ namespace AccountModule
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("MyCalendar");
+            Console.WriteLine("1. Create account");
+            Console.WriteLine("2. Log in");
+            int action = Convert.ToInt32(Console.ReadLine());
+            switch (action)
+            {
+                case 1:
+                    CreateAccount();
+                    break;
+                case 2:
+                    Login();
+                    break;
+                default:
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+
+        static void CreateAccount()
+        {
+            Console.WriteLine("Insert login and password.");
+            string login = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            Console.WriteLine("login: {0}\npassword: {1}", login, password);
+
+        }
+
+        static void Login()
+        {
+            Console.WriteLine("Insert login and password.");
+            string login = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            Console.WriteLine("login: {0}\npassword: {1}", login, password);
         }
     }
 }
