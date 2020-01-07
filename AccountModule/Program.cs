31499f1dbf95119fe27a9eae913e542887a096bb
Wojciech Default <wojciechklanowski@gmail.com>
2020-01-07 16:37:07 +0100

    login feature add

--- AccountModule/Program.cs
+++ AccountModule/Program.cs
@@ -63,7 +63,25 @@ namespace AccountModule
             string login = Console.ReadLine();
             string password = Console.ReadLine();
 
-            Console.WriteLine("login: {0}\npassword: {1}", login, password);
+            string connectionString = @"Server=localhost;Database=MyCalendar;Trusted_Connection=True";
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                using (SqlCommand sqlCommand = new SqlCommand())
+                {
+                    sqlCommand.Connection = sqlConnection;
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.CommandText = "usp_GetCalendarUser_Id";
+
+                    sqlCommand.Parameters.Add(new SqlParameter("@p_Name", login));
+                    sqlCommand.Parameters.Add(new SqlParameter("@p_Password", password));
+
+                    sqlCommand.ExecuteNonQuery();
+                    //odbieranie danych
+                }
+            }
         }
     }
 }
