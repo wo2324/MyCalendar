28d0cf1206c2d0af97bd25d7ada3db863d261c01
Wojciech Default <wojciechklanowski@gmail.com>
2020-01-08 17:20:45 +0100

    login feature improve

--- AccountModule/Program.cs
+++ AccountModule/Program.cs
@@ -42,45 +42,64 @@ namespace AccountModule
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
                 sqlConnection.Open();
-
                 using (SqlCommand sqlCommand = new SqlCommand())
                 {
                     sqlCommand.Connection = sqlConnection;
                     sqlCommand.CommandType = CommandType.StoredProcedure;
                     sqlCommand.CommandText = "usp_CalendarUserAdd";
-
                     sqlCommand.Parameters.Add(new SqlParameter("@p_Name", login));
                     sqlCommand.Parameters.Add(new SqlParameter("@p_Password", password));
-
                     sqlCommand.ExecuteNonQuery();
                 }
             }
         }
 
-        static void Login()
+        static DataSet Login()
         {
             Console.WriteLine("Insert login and password.");
             string login = Console.ReadLine();
             string password = Console.ReadLine();
 
+            DataSet dataSet = new DataSet();
             string connectionString = @"Server=localhost;Database=MyCalendar;Trusted_Connection=True";
 
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            try
             {
-                sqlConnection.Open();
-
-                using (SqlCommand sqlCommand = new SqlCommand())
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
                 {
-                    sqlCommand.Connection = sqlConnection;
-                    sqlCommand.CommandType = CommandType.StoredProcedure;
-                    sqlCommand.CommandText = "usp_GetCalendarUser_Id";
+                    sqlConnection.Open();
+                    using (SqlCommand sqlCommand = new SqlCommand())
+                    {
+                        sqlCommand.Connection = sqlConnection;
+                        sqlCommand.CommandType = CommandType.StoredProcedure;
+                        sqlCommand.CommandText = "usp_GetCalendarUser_Id";
+                        sqlCommand.Parameters.Add(new SqlParameter("@p_Name", login));
+                        sqlCommand.Parameters.Add(new SqlParameter("@p_Password", password));
+                        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
 
-                    sqlCommand.Parameters.Add(new SqlParameter("@p_Name", login));
-                    sqlCommand.Parameters.Add(new SqlParameter("@p_Password", password));
+                        if (sqlConnection.State == ConnectionState.Closed)
+                        {
+                            sqlConnection.Open();
+                        }
 
-                    sqlCommand.ExecuteNonQuery();
-                    //odbieranie danych
+                        sqlDataAdapter.Fill(dataSet);
+
+                        if (dataSet.Tables.Count != 0)
+                        {
+                            Console.WriteLine(dataSet.Tables[0].Rows[0]["CalendarUser_Id"]);
+                            return dataSet;
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
                 }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                return null;
             }
         }
     }
