3fa6a0c432a848610c566cdab8dc2fd435fa3142
Wojciech Default <wojciechklanowski@gmail.com>
2020-01-25 13:59:40 +0100

    grid testing

---
+++ MyCalendar/Planner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalendar
+{
+    public class PlannerElement
+    {
+        public int Monday { get; set; }
+        public int Wtorek { get; set; }
+        public int Sroda { get; set; }
+
+        public PlannerElement(int Monday, int Wtorek, int Sroda)
+        {
+            this.Monday = Monday;
+            this.Wtorek = Wtorek;
+            this.Sroda = Sroda;
+        }
+    }
+}
