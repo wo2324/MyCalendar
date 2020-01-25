05fc92fbf92cbfecc98dd6c061215e8ba272d35b
Wojciech Default <wojciechklanowski@gmail.com>
2020-01-25 18:53:28 +0100

    planner dev

---
+++ MyCalendar/Planner3.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyCalendar.Test
+{
+    /// <summary>
+    /// Interaction logic for Planner3.xaml
+    /// </summary>
+    public partial class Planner3 : Window
+    {
+        ObservableCollection<PlannerElement> Elements = new ObservableCollection<PlannerElement>();
+
+        public Planner3()
+        {
+            InitializeComponent();
+
+            Elements.Add(new PlannerElement("12:15",
+                "meal", "meal", "meal", "meal",
+                "meal", "meal", "meal"));
+            Elements.Add(new PlannerElement("12:30", 
+                "work", "work", "work", "study",
+                "study", "study", "chill"));
+            Elements.Add(new PlannerElement("13:15",
+                "work", "work", "work", "study",
+                "study", "study", "chill"));
+
+            PlannerDataGrid.ItemsSource = Elements;
+        }
+    }
+}
--- MyCalendar/PlannerElement.cs
+++ MyCalendar/PlannerElement.cs
@@ -8,10 +8,27 @@ namespace MyCalendar
 {
     public class PlannerElement
     {
-
-        public PlannerElement()
+        public string Time { get; set; }
+        public string MondayActivity { get; set; }
+        public string TuesdayActivity { get; set; }
+        public string WednesdayActivity { get; set; }
+        public string ThursdayActivity { get; set; }
+        public string FridayActivity { get; set; }
+        public string SaturdayActivity { get; set; }
+        public string SundayActivity { get; set; }
+        //Do niego wchodzą klasy zawierające pełne informacje o zadaniu
+        public PlannerElement(string Time,
+            string MondayActivity, string TuesdayActivity, string WednesdayActivity, string ThursdayActivity,
+            string FridayActivity, string SaturdayActivity, string SundayActivity)
         {
-
+            this.Time = Time;
+            this.MondayActivity = MondayActivity;
+            this.TuesdayActivity = TuesdayActivity;
+            this.WednesdayActivity = WednesdayActivity;
+            this.ThursdayActivity = ThursdayActivity;
+            this.FridayActivity = FridayActivity;
+            this.SaturdayActivity = SaturdayActivity;
+            this.SundayActivity = SundayActivity;
         }
     }
 }
