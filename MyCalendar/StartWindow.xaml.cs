0c41fb39d5be1cbc18816b1b3ed2cad479e418a2
Wojciech Default <wojciechklanowski@gmail.com>
2020-01-27 22:15:43 +0100

    login handle

--- MyCalendar/StartWindow.xaml.cs
+++ MyCalendar/StartWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +25,61 @@ namespace MyCalendar
         public StartWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string login = Login.Text;
+            string password = Password.Password;
+
+            LoginProcess(login, password);
+        }
+
+        static void LoginProcess(string login, string password)
+        {
+            DataSet dataSet = new DataSet();
+
+            try
+            {
+                string connectionString = "Server=localhost;Database=MyCalendar;Trusted_Connection=True";
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    sqlConnection.Open();
+                    using (SqlCommand sqlCommand = new SqlCommand())
+                    {
+                        sqlCommand.Connection = sqlConnection;
+                        sqlCommand.CommandType = CommandType.StoredProcedure;
+                        sqlCommand.CommandText = "mc.usp_ParticipantGet";
+                        sqlCommand.Parameters.Add(new SqlParameter("@p_Participant_Name", login));
+                        sqlCommand.Parameters.Add(new SqlParameter("@p_Participant_Password", password));
+                        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                        if (sqlConnection.State == ConnectionState.Closed)
+                        {
+                            sqlConnection.Open();
+                        }
+
+                        sqlDataAdapter.Fill(dataSet);
+
+                        if (dataSet.Tables[0].Rows.Count != 0)
+                        {
+                            Console.WriteLine("Logged in as a user {0}.", dataSet.Tables[0].Rows[0]["Participant_Id"]);
+                            
+                            //return dataSet;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Bad user name or password.");
+                            //return null;
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                //return null;
+            }
         }
     }
 }
